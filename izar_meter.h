b90ff0a89af6849c0d4a011cf46d72e766e6c5eb
MariuszWoszczynski <115429759+MariuszWoszczynski@users.noreply.github.com>
2022-12-13 14:19:22 +0100

    Update izar_meter.h

--- izar_meter.h
+++ izar_meter.h
@@ -3,32 +3,43 @@
 #include "ArduinoJson.h"
 #include "izar_wmbus.h"
 
-//uint32_t meterId = 0xFFFFFFFF;                          //comment to determine the ID of your counter, enter the ID and uncomment the line 
- 
+
+//int  meterId = 0xFFFFFFFF;      //comment to determine the ID of your counter, enter the ID and uncomment the line 
+
+int waterUsage_temp;
+int waterUsage;
 IzarWmbus reader;
 
-class MyCustomSensor : public Component {
+class MyCustomSensor : public Component, public TextSensor  {
  public:
-  Sensor *water_sensor = new Sensor();                            
-  Sensor *id_sensor = new Sensor();                              
+  TextSensor *water_sensor = new TextSensor();   
+  TextSensor *id_sensor = new TextSensor();
+                       
   
   void setup() {
-       reader.init(0);                                    //to display all meters in range
-//       reader.init(meterId);                            //uncomment the line after identifying your meter
+       reader.init(0);                                //to display all meters in range
+//       reader.init(meterId);                        //uncomment the line after identifying your metera
   }
   
 IzarResultData data;
-StaticJsonDocument<200> doc;
 
   void loop() {
 
     FetchResult result = reader.fetchPacket(&data);
     if (result == FETCH_SUCCESSFUL) {
-        int waterUsage = data.waterUsage;                      
-        water_sensor->publish_state(waterUsage);
+        waterUsage_temp = data.waterUsage;
+        if ((waterUsage_temp > 0) and (waterUsage_temp < 10000000)) {    //data filter
+            waterUsage = waterUsage_temp;
+        } 
+        char usage_text[32];
+        itoa(waterUsage, usage_text, 10);
+        water_sensor->publish_state(usage_text);
+        
+        int meterId = data.meterId;                //Uncomment from reading all the meters in the range. Comment this line after identifying your meter ID.              
 
-        uint32_t meterId = data.meterId;                //Uncomment from reading all the meters in the range. Comment this line after identifying your meter ID.             
-        id_sensor->publish_state(meterId);
+        char ID_text[32];
+        itoa(meterId, ID_text, 16);
+        id_sensor->publish_state(ID_text);
 
     } else {
         delay(300);
@@ -36,7 +47,5 @@ StaticJsonDocument<200> doc;
     }
   }
 };
-
-
 
 
