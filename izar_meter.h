1a6d938ea8349c9360e171e6666ff510010e5c64
MariuszWoszczynski <115429759+MariuszWoszczynski@users.noreply.github.com>
2022-12-13 16:14:54 +0100

    Update izar_meter.h

--- izar_meter.h
+++ izar_meter.h
@@ -4,21 +4,27 @@
 #include "izar_wmbus.h"
 
 
-//int  meterId = 0xFFFFFFFF;      //comment to determine the ID of your counter, enter the ID (instead FFFFFFFF) and uncomment the line 
+int  meterId = 0xFFFFFFFF;      //comment to determine the ID of your counter, enter the ID (instead FFFFFFFF) and uncomment the line 
 
 int waterUsage_temp;
 int waterUsage;
 IzarWmbus reader;
 
-class MyCustomSensor : public Component, public TextSensor  {
- public:
-  TextSensor *water_sensor = new TextSensor();   
-  TextSensor *id_sensor = new TextSensor();
-                       
-  
+namespace MyTextData {
+      TextSensor *my_text_sensor = new TextSensor();
+    }
+    
+class MyTextSensor :public Component, public TextSensor {
+      public:
+        TextSensor *my_text_sensor = MyTextData::my_text_sensor;
+    };
+    
+class MySensor :public Component, public Sensor {
+      public: Sensor *my_sensor = new Sensor();
+      
   void setup() {
-       reader.init(0);                                //to display all meters in range
-//       reader.init(meterId);                        //uncomment the line after identifying your metera
+//       reader.init(0);                                 //to display all meters in range
+       reader.init(meterId);                             //uncomment the line after identifying your meter
   }
   
 IzarResultData data;
@@ -28,24 +34,21 @@ IzarResultData data;
     FetchResult result = reader.fetchPacket(&data);
     if (result == FETCH_SUCCESSFUL) {
         waterUsage_temp = data.waterUsage;
-        if ((waterUsage_temp > 0) and (waterUsage_temp < 10000000)) {    //data filter
+        if ((waterUsage_temp > 0) and (waterUsage_temp < 10000000)) {     //data filter
             waterUsage = waterUsage_temp;
         } 
-        char usage_text[32];
-        itoa(waterUsage, usage_text, 10);
-        water_sensor->publish_state(usage_text);
-        
-        int meterId = data.meterId;                //Uncomment from reading all the meters in the range. Comment this line after identifying your meter ID.              
-
+       
+//        int meterId = data.meterId;                 //Uncomment from reading all the meters in the range. Comment this line after identifying your meter ID.                            
         char ID_text[32];
         itoa(meterId, ID_text, 16);
-        id_sensor->publish_state(ID_text);
-
+       
+        my_sensor->publish_state(waterUsage);
+        MyTextData::my_text_sensor->publish_state(ID_text);
+      
     } else {
         delay(300);
         reader.ensureRx(); 
     }
   }
 };
-
 
